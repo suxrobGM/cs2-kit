0cc170b88f08bbce477cdde1e0cede85ba49f54d
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-02-17 08:55:59 -0500

    Fix documentation comment formatting in ICommandCaller interface

--- include/CS2Kit/Commands/ICommandCaller.hpp
+++ include/CS2Kit/Commands/ICommandCaller.hpp
@@ -7,7 +7,7 @@ namespace CS2Kit::Commands
 {
 
 /**
- * @brief  representation of a command caller (player or server console).
+ * @brief Interface representation of a command caller (player or server console).
  * Consumers implement this to wrap their Player class or console representation.
  */
 class ICommandCaller
